f1784773a25591bb20f99126be070d9e979d9c07
Matthammond96 <ultramatt96@hotmail.co.uk>
2021-01-06 16:37:00 +0000

    Update PropertyHighlights.js

--- src/View/Components/Properties/PropertyHighlights.js
+++ src/View/Components/Properties/PropertyHighlights.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import Highlight from "./Highlight";
 
@@ -16,11 +16,18 @@ class PropertyHighlights extends Component {
       <div>
         {this.state.component.fields.aboveHighlights.content.map(el => documentToReactComponents(el))}  
 
-        <div className="highlight-container">
-          {this.state.component.fields.propertyTable.map(el => <Highlight id={el.sys.id}></Highlight>)}
-        </div>
-
-        {this.state.component.fields.belowHighlights.content.map(el => documentToReactComponents(el))}  
+        {this.state.component.fields.propertyTable &&
+          <div className="highlight-container">
+            {this.state.component.fields.propertyTable.map(el => <Highlight id={el.sys.id}></Highlight>)}
+          </div>
+        }
+        
+        {this.state.component.fields.belowHighlights &&
+          <Fragment>
+            {this.state.component.fields.belowHighlights.content.map(el => documentToReactComponents(el))}
+          </Fragment>
+        }
+        
       </div>
     )
   }
